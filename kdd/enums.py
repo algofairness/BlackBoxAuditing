5af2d7be77111f39f26ef4490cfc9e5d948f4624
h205c <h205c@hoover.cs.haverford.edu>
2017-06-12 11:04:07 -0400

    Removed kdd dir from repo

--- kdd/enums.py
+++
@@ -1,6 +0,0 @@
-class Enumeration:
-    def __init__(self, enums):
-        for i, enum in enumerate(enums):
-            setattr(self, enum, i)
-
-repair_type = Enumeration(["COMBINATORIAL", "GEOMETRIC"])
