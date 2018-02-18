afca7bef01e5d5fc96a849cf19da492ce29e2d09
Charlie Marx <charliemarx@Charlies-MacBook-Pro.local>
2018-02-18 11:03:12 -0500

    preparing for push to master

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
 NAME ='BlackBoxAuditing'
-VERSION = '0.1.29'
+VERSION = '0.1.30'
 
 DESCRIPTION = 'Sample Implementation of Gradient Feature Auditing (GFA)'
 LONG_DESCRIPTION = long_description
